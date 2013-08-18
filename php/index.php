daaabcdb14afaca8eecf1d71abe15a2946d92374
OpenShift guest <jarias14@gmail.com>
2013-08-17 20:28:33 -0400

    cople of things

--- php/index.php
+++ php/index.php
@@ -2,69 +2,7 @@
 <html lang="en">
     <head>
         <meta http-equiv="refresh" content="0;URL='html/pre-wedding/index.html'" />    
-
-		<!-- ==============================================
-		Title and basic Metas
-		=============================================== -->
-		<meta charset="utf-8">
-		<title>The Big Day</title>
-		<meta name="description" content="The Big Day is a Responsive One-Page Wedding Template">
-		<meta name="author" content="G10v3">
-
-		<!-- ==============================================
-		Mobile Metas
-		=============================================== -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-		<!-- ==============================================
-		CSS
-		=============================================== -->
-		<link rel="stylesheet" href="assets/bootstrap.min.css" /> <!-- Twitter Bootstrap -->
-		<link rel="stylesheet" href="assets/bootstrap-responsive.min.css" /> <!-- Twitter Bootstrap Responsive -->
-		<link rel="stylesheet" href="assets/style.css"> <!-- Main stylesheet -->
-		
-		<!--[if lt IE 9]>
-			<script src="assets/html5.js"></script>
-		<![endif]-->
-
-		<!-- ==============================================
-		Fonts
-		=============================================== -->
-		<link href='http://fonts.googleapis.com/css?family=Handlee' rel='stylesheet' type='text/css'>
-
-		<!-- ==============================================
-		Favicons
-		=============================================== -->
-		<link rel="shortcut icon" href="assets/favicon.ico">
-		<link rel="apple-touch-icon" href="assets/apple-touch-icon.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="assets/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="assets/apple-touch-icon-114x114.png">
-		
-
-		
 	</head>
 	<body>
-
-		<section class="header">
-			<div class="container">
-				<div class="row">
-					<div class="span12">
-						<h1>Our Big Day's!</h1>
-						<h2>Welcome to our big day's website. You'll be redirected to the main site in a second..</h2>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<section class="footer">
-			<div class="container">
-				<div class="row">
-					<div class="span12">
-						<p>..</p>
-					</div>
-				</div>
-			</div>
-		</section>
-
 	</body>
 </html>
